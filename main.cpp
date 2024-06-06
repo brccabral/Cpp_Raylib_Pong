0ed58a858b36098ce301f3fa87a0328cec84f87d
Bruno Cabral <cabral.brc@gmail.com>
2024-06-06 12:37:32 -0700

    ball boundaries
    Signed-off-by: Bruno Cabral <cabral.brc@gmail.com>

--- main.cpp
+++ main.cpp
@@ -14,8 +14,17 @@ public:
     }
     void Update()
     {
-            x += speed_x;
+        x += speed_x;
         y += speed_y;
+
+        if (y + radius >= GetScreenHeight() || y - radius < 0)
+        {
+            speed_y *= -1;
+        }
+        if (x + radius >= GetScreenWidth() || x - radius < 0)
+        {
+            speed_x *= -1;
+        }
     }
 };
 
