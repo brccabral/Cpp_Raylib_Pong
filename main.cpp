698fd07e2913ebb24c00e81ea6428400e3fcaee4
Bruno Cabral <cabral.brc@gmail.com>
2024-06-06 12:14:27 -0700

    draw game elements

--- main.cpp
+++ main.cpp
@@ -2,12 +2,21 @@
 
 int main()
 {
-    InitWindow(800, 450, "raylib - basic window");
+    const int screen_width = 1280;
+    const int screen_height = 800;
+    InitWindow(screen_width, screen_height, "Pong");
+    SetTargetFPS(60);
+
     while (!WindowShouldClose())
     {
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+
+        // Drawing
+        DrawLine(screen_width / 2, 0, screen_width / 2, screen_height, WHITE);
+        DrawCircle(screen_width / 2, screen_height / 2, 20, WHITE);
+        DrawRectangle(10, screen_height / 2 - 60, 25, 120, WHITE);
+        DrawRectangle(screen_width - 25 - 10, screen_height / 2 - 60, 25, 120, WHITE);
+
         EndDrawing();
     }
     CloseWindow();
