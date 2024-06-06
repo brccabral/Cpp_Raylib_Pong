4449f5c053fd02eca9340e61d6ba65cffc88589c
Bruno Cabral <cabral.brc@gmail.com>
2024-06-06 12:41:36 -0700

    player draw
    Signed-off-by: Bruno Cabral <cabral.brc@gmail.com>

--- main.cpp
+++ main.cpp
@@ -30,6 +30,22 @@ public:
 
 Ball ball;
 
+class Paddle
+{
+public:
+
+    Paddle() : x(10), y(10), width(25), height(120), speed(7){};
+    float x, y;
+    float width, height;
+    float speed;
+    void Draw() const
+    {
+        DrawRectangle(x, y, width, height, WHITE);
+    }
+};
+
+Paddle player;
+
 int main()
 {
     constexpr int screen_width = 1280;
@@ -43,6 +59,12 @@ int main()
     ball.speed_x = 7.0f;
     ball.speed_y = 7.0f;
 
+    player.width = 25;
+    player.height = 120;
+    player.x = screen_width - player.width - 10;
+    player.y = screen_height /2 - player.height/2;
+    player.speed = 6;
+
     while (!WindowShouldClose())
     {
         ball.Update();
@@ -54,7 +76,7 @@ int main()
         DrawLine(screen_width / 2, 0, screen_width / 2, screen_height, WHITE);
         ball.Draw();
         DrawRectangle(10, screen_height / 2 - 60, 25, 120, WHITE);
-        DrawRectangle(screen_width - 25 - 10, screen_height / 2 - 60, 25, 120, WHITE);
+        player.Draw();
 
         EndDrawing();
     }
